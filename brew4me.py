fb8c399b8495e925adcc285190912761e0623120
mike <mikeliu0512@gmail.com>
2022-08-24 17:19:47 -0700

    Consolidated remove_duplicates into get_collection

--- brew4me.py
+++ brew4me.py
@@ -11,16 +11,26 @@ from telnetlib import NOOPT
 class deck:
     
     def __init__(self,deck_dir,deck_file):
+        """Creates a deck object from:
+        1.location of decks
+        2.name of deck file 
+        """
         self.name = deck_file[:-4]
         self.mainboard = []
         self.has_sideboard = False
         self.sideboard = []
         self.set_deck_from_dir (deck_dir+deck_file)
 
-    def get_mainboard (self):
-        return self.mainboard
-
     def set_deck_from_dir  (self,deck_dir):
+        """Populates the deck object's main deck and sideboard.
+        Deck files contain n entries in the format "quantity cardname"
+        Main deck and sideboard saved in the following format:
+        [[count1,cardname1]
+        ,[count2,cardname2]
+        ,[count3,cardname3]
+        , ......]
+        Note: card quantities are saved as strings.
+        """
         f = open(deck_dir, 'r')
         count = 0
         result = []
@@ -35,7 +45,7 @@ class deck:
                 continue
             if (int(cur[0])):
                 if (self.has_sideboard == True):
-                    self.sideboard.append([ cur[0], " ".join(cur[1:]) ])
+                    self.sideboard.append([ cur[0], " ".join(cur[1:])])
                 else:
                     self.mainboard.append([cur[0]," ".join(cur[1:])])
             else:
@@ -43,48 +53,58 @@ class deck:
                     self.sideboard.append([cur[0]," ".join(cur[1:])])
                 else:
                     self.mainboard.append([1," ".join(cur)])
-        
         f.close()
         return result
 
-    def get_sideboard (self,file_dir):
-        f = open(file_dir)
+    def get_mainboard (self):
+        return self.mainboard
 
-        f.close()
+    def get_sideboard (self,file_dir):
+        return self.sideboard
 
 class collection:
     cards = []
 
     def __init__(self,collection_dir):
+        """Makes collection of cards from a collection file's path"""
         self.get_collection(collection_dir)
         self.cards = self.cards [1:]
-        self.remove_duplicates()
+        #self.remove_duplicates()
         
-    def remove_duplicates (self):
-        count = 0
-        while (count < (len(self.cards)-1) ):
-            try:
-                if self.cards[count][1] == self.cards[count+1][1] :
-                    self.cards[count][0] = str( 
-                        int(self.cards[count][0]) +
-                        int(self.cards[count+1][0])
-                        )
-                    self.cards.pop(count+1)
-                else:
-                    count += 1  
-            except:
-                count += 1 
+   
             
     def get_collection (self,file_dir):
+        """Populates collection with list entries in the following
+        format:
+        [[count1,cardname1]
+        ,[count2,cardname2]
+        ,[count3,cardname3]
+        , ......]
+        Note: card quantities are saved as strings.
+        """
         with open(file_dir, 'r') as file:
             reader = csv.reader(file)
             for row in reader:
                 try:
-                    self.cards.append([row[0],row[2]])
+                    if self.cards:
+                        if self.cards[-1][1] == row[2]:
+                            self.cards[-1][0] = str(int(self.cards[-1][0])
+                                                    + int(row[0]))
+                        else:
+                            self.cards.append([row[0],row[2]])
+                    else:
+                        self.cards.append([row[0],row[2]])
                 except:
                     continue
 
 def compare_deck_to_collection (collection,deck):
+    """Returns a compared deck in the format:
+    [[owned1#,needed1#,cardname1]
+    ,[owned2#,needed2#,cardname2]
+    ,[owned3#,needed3#,cardname3]
+    , ......]
+     Note: card quantities are saved as strings.
+    """
     result = []
     for i in deck.mainboard:
         result.append(["0"]+i)
@@ -92,8 +112,6 @@ def compare_deck_to_collection (collection,deck):
             if i[1] == j[1]:
                 result[-1][0] = j[0]
     return result
-
-
       
 def main():
 
@@ -123,6 +141,7 @@ def main():
                 or j[2] == "Mountain"
                 or j[2] == "Forest"
             ):
+            #Program assumes user owns sufficient basic lands.
                 pass
             else:
                 total_cards += float(j[1])
@@ -134,7 +153,9 @@ def main():
     results = sorted(results, key=lambda x: x[-1])
     for i in results :     
         print ("\n================================\n")
-        print ("Deck: "+i[0]+"\nCompletion rate (Not counting basic lands): " +str(i[-1]*100)+"%\n")
+        print ("Deck: " + i[0] 
+               + "\nCompletion rate (Not counting basic lands): " 
+               + str(i[-1]*100)+"%\n")
         for j in i[1]:
             print(j[0]+" | "+j[1]+" | "+j[2])
     input("Press Enter to exit")
